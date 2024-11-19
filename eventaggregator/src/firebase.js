1b1bd50f374aab6457b2c5b7f13fb4607f3f7083
Richie Prak <legobigchill@gmail.com>
2024-11-18 21:42:18 -0800

    Restored Firebase

--- eventaggregator/src/firebase.js
+++ eventaggregator/src/firebase.js
@@ -11,14 +11,18 @@ import { getFirestore } from "@firebase/firestore";
 const firebaseConfig = {
   apiKey: "AIzaSyDqgtkpby4KM9jJN6dESZ2hLUuYnAFq63o",
   authDomain: "event-aggregator-b95e6.firebaseapp.com",
+  databaseURL: "https://event-aggregator-b95e6-default-rtdb.firebaseio.com",
   projectId: "event-aggregator-b95e6",
   storageBucket: "event-aggregator-b95e6.firebasestorage.app",
   messagingSenderId: "570659202335",
-  appId: "1:570659202335:web:a3358cbe03b63440993c2a",
-  measurementId: "G-3ZJNGQZLBS"
+  appId: "1:570659202335:web:cfd9e1f2d96075c6993c2a",
+  measurementId: "G-M10TZN2EVK"
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 export const firestore = getFirestore(app);
+
+
