89571b9b774323fc5e806382cb25f7a6e7f9ace7
Richie Prak <legobigchill@gmail.com>
2024-12-03 16:11:09 -0800

    Added profile header details and profile picture

--- eventaggregator/src/Pages/Profile/Profile.jsx
+++ eventaggregator/src/Pages/Profile/Profile.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import './Profile.css'
 import Sidebar from '../../Components/Sidebar/Sidebar'
-import headerimage from '../../assets/profile-header-image.jpg';
+import headerimage from '../../assets/profile-header-image.png';
+import profileimage from '../../assets/profile-picture.png';
 
 export const Profile = ({ sidebar }) => {
     return (
@@ -9,19 +10,25 @@ export const Profile = ({ sidebar }) => {
             <Sidebar sidebar={sidebar} />
             <div className={`container ${sidebar ? "" : 'large-container'}`}>
                 <div className="profile-header">
-                   
-                    <div className="profile-header-top">
+                    <div className="profile-banner">
+                        <div className="profile-banner-sizer">
+                            <img className="profile-banner-image" src={headerimage} alt=""></img>
+                        </div>
+                        <img className="profile-picture" src={profileimage} alt=""></img>
+                    </div>
+                    <div className="profile-header-content">
                         <div className="header-caption">
                             <div className="header-names">
-                                <h1>Rizzo</h1>
+                                <h1>Freakbob</h1>
                             </div>
                             <div className="header-details">
+                                <h2><span>30</span> Friends</h2>
+                                <h2><span>30</span> Posts</h2>
                             </div>
                         </div>
                         <div className="profile-header-more">
                         </div>
                     </div>
-                    <img className="profile-header-image" src={headerimage} alt = ""></img>
                 </div>
                 <div className="profile-nav">
 
