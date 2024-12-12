33ab63f75ab9c2dd122168def1778ee67dd4da87
Richie Prak <legobigchill@gmail.com>
2024-12-11 19:40:44 -0800

    Added recommended events page

--- eventaggregator/src/Pages/EventCategory/EventCategory.jsx
+++ eventaggregator/src/Pages/EventCategory/EventCategory.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import Sidebar from '../../Components/Sidebar/Sidebar'
 import Events from '../../Components/Events/Events'
+import { useParams } from 'react-router-dom'
 
 export const EventCategory = ({ sidebar }) => {
+
+    const {categoryName} = useParams();
+
     return (
         <>
             <Sidebar sidebar={sidebar} />
             <div className={`container ${sidebar ? "" : 'large-container'}`}>
-                <Events/>
+                <Events categoryName={categoryName}/>
             </div>
         </>
     )
