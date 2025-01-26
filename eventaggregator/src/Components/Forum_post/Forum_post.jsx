08d7b0a396852855280ce2cc6ad66fe8b1cc6ce2
Leo Roth <leoroth123@yahoo.com>
2025-01-25 20:52:28 -0800

    Created a forum_post component
    - this component can be re-used on the profile tab as well as other forum pages

---
+++ eventaggregator/src/Components/Forum_post/Forum_post.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import './Forum_post.css';
+
+const Forum_post = ({ event, title, preview, author, timestamp, upvotes, downvotes, replies, thumbnail }) => {
+  return (
+    <div className="forum-post">
+      <div className="post-thumbnail">
+        <img src={thumbnail || "/api/placeholder/64/64"} alt="" />
+      </div>
+      
+      <div className="post-content">
+        <span className="post-event">{event}</span>
+        <div className="title-box">
+          <h3 className="post-title">{title}</h3>
+        </div>
+        <div className="post-info">
+          <div className="post-meta">
+            <span>{timestamp} • {author}</span>
+          </div>
+          
+          <div className="post-stats">
+            <div className="votes">
+              <span className="upvotes">{upvotes}↑</span>
+              <span className="downvotes">{downvotes}↓</span>
+            </div>
+            <div className="replies">
+              See {replies} Replies
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="preview-box">
+        <p className="post-preview">{preview}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Forum_post;
--- eventaggregator/src/Pages/Forum/Forum.jsx
+++ eventaggregator/src/Pages/Forum/Forum.jsx
@@ -1,19 +1,48 @@
-import './Forum.css'
-import Sidebar from '../../Components/Sidebar/Sidebar'
+import React from 'react';
+import './Forum.css';
+import Sidebar from '../../Components/Sidebar/Sidebar';
+import Forum_post from '../../Components/Forum_post/Forum_post';
+/* 
+*  below this comment is the temporary use of the imported image to fill the thumbnail
+*  until we incorporate the posting functionality where the user can choose the thumbnail
+*  then we can just load it in from the database (remove this comment once implemented)
+*/
+import thumbnail1 from '../../assets/thumbnail1.png';
 
 
 export const Forum = ({ sidebar }) => {
+    // Example post data - replace with the database data once that is set up then remove this comment please :)
+    const posts = [  // the info used in this 'posts' array is used to fill in the Forum_post component
+        {
+            event: "YFP VS Fly Quest",
+            title: "Recap discussion of the League match last night. I can't believe theBausffs man! Why would he do that?",
+            preview: "What an intense match! The teamfight at Baron really turned things around. I noticed theBausffs goofing " + 
+            "off in the top lane seemingly tricking the enemy team in his own match even though he was supposed to be casting " +
+            "this game for Fly Quest, they must be very annoyed at him. Was he sponsored? If so he is most-definitely losing " +
+            "the sponsorship or at least they are going to reprimand him somehow, that is just irrespopnsible to be honest. He " +
+            "probably made so much money just for him to not even pay attention, like wow.",
+            author: "TheLegend27",
+            timestamp: "50 mins ago",
+            upvotes: 7,
+            downvotes: 1,
+            replies: 7,
+            thumbnail: thumbnail1
+        }
+    ];
+
     return(
         <>
           <Sidebar sidebar={sidebar} />
           <div className={`container ${sidebar ? "" : 'large-container'}`}>
             <div className="forum-feed">
-                <h1>Forum</h1>
+                <h1>Recommended</h1>
+                {posts.map((post, index) => (
+                    <Forum_post key={index} {...post} />
+                ))}
             </div>
           </div>
         </>
     )
-
-
-
 }
+
+export default Forum;
