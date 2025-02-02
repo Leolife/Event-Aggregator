f6ce60ddbb0fb610f8b21c462027a15b135997ea
Richie Prak <legobigchill@gmail.com>
2025-02-01 18:52:18 -0800

    Added tags to event tiles

--- eventaggregator/src/Pages/Home/Home.jsx
+++ eventaggregator/src/Pages/Home/Home.jsx
@@ -2,20 +2,10 @@
 import React, { useState } from 'react'
 import './Home.css'
 import Sidebar from '../../Components/Sidebar/Sidebar'
-import thumbnail1 from '../../assets/thumbnail1.png'
-import thumbnail2 from '../../assets/thumbnail2.jpg'
-import thumbnail3 from '../../assets/thumbnail3.png'
-import thumbnail4 from '../../assets/thumbnail4.jpg'
-import thumbnail5 from '../../assets/thumbnail5.png'
-import thumbnail6 from '../../assets/thumbnail6.png'
-import thumbnail7 from '../../assets/thumbnail7.jpg'
-import thumbnail8 from '../../assets/thumbnail8.png'
-import thumbnail9 from '../../assets/thumbnail9.jpg'
-import thumbnail10 from '../../assets/thumbnail10.png'
-import thumbnail11 from '../../assets/thumbnail11.jpg'
-import thumbnail12 from '../../assets/thumbnail12.png'
 import { ReactComponent as SearchIcon } from '../../assets/search-icon.svg';
-import { Link } from 'react-router-dom'
+import { eventInfo } from './TempEvents'
+import EventTile from '../../Components/Events/Event_tile'
+
 
 export const Home = ({ sidebar }) => {
   const [isActive, setActive] = useState(false)
@@ -64,93 +54,10 @@ export const Home = ({ sidebar }) => {
           </div>
         </div>
         <div className={`container ${sidebar ? "" : 'large-container'}`}>
-
           <div className="feed">
-            <Link to={'event/category/league-of-legends'} className='card'>
-              <div className='img-sizer'>
-                <img src={thumbnail1} alt="" />
-              </div>
-              <h2> League of Legends </h2>
-              <h3> 25 Events • 6 Upcoming </h3>
-            </Link>
-            <div className='card'>
-              <div className='img-sizer'>
-                <img src={thumbnail2} alt="" />
-              </div>
-              <h2> Marvel Rivals </h2>
-              <h3> 25 Events • 6 Upcoming </h3>
-            </div>
-            <div className='card'>
-              <div className='img-sizer'>
-                <img src={thumbnail3} alt="" />
-              </div>
-              <h2> Music </h2>
-              <h3> 25 Events • 6 Upcoming </h3>
-            </div>
-            <div className='card'>
-              <div className='img-sizer'>
-                <img src={thumbnail4} alt="" />
-              </div>
-              <h2> Super Smash Bros... </h2>
-              <h3> 25 Events • 6 Upcoming </h3>
-            </div>
-            <div className='card'>
-              <div className='img-sizer'>
-                <img src={thumbnail5} alt="" />
-              </div>
-              <h2> Sports (Fake) </h2>
-              <h3> 25 Events • 6 Upcoming </h3>
-            </div>
-            <div className='card'>
-              <div className='img-sizer'>
-                <img src={thumbnail6} alt="" />
-              </div>
-              <h2> Valorant </h2>
-              <h3> 25 Events • 6 Upcoming </h3>
-            </div>
-            <div className='card'>
-              <div className='img-sizer'>
-                <img src={thumbnail7} alt="" />
-              </div>
-              <h2> VRChat </h2>
-              <h3> 25 Events • 6 Upcoming </h3>
-            </div>
-            <div className='card'>
-              <div className='img-sizer'>
-                <img src={thumbnail8} alt="" />
-              </div>
-              <h2> Hololive </h2>
-              <h3> 25 Events • 6 Upcoming </h3>
-            </div>
-            <div className='card'>
-              <div className='img-sizer'>
-                <img src={thumbnail9} alt="" />
-              </div>
-              <h2> Osu! </h2>
-              <h3> 25 Events • 6 Upcoming </h3>
-            </div>
-            <div className='card'>
-              <div className='img-sizer'>
-                <img src={thumbnail10} alt="" />
-              </div>
-              <h2> Magic the Gathering </h2>
-              <h3> 25 Events • 6 Upcoming </h3>
-            </div>
-            <div className='card'>
-              <div className='img-sizer'>
-                <img src={thumbnail11} alt="" />
-              </div>
-              <h2> Pokemon TCG </h2>
-              <h3> 25 Events • 6 Upcoming </h3>
-            </div>
-            <div className='card'>
-              <div className='img-sizer'>
-                <img src={thumbnail12} alt="" />
-              </div>
-              <h2> One Piece TCG </h2>
-              <h3> 25 Events • 6 Upcoming </h3>
-            </div>
-
+          {eventInfo.map((tile, index) => (
+                    <EventTile key={index} {...tile} />
+                ))}
           </div>
         </div>
       </div>
