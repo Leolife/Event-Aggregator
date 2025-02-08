e92969277c63ebd4204d15032695bef98440efc8
Leo Roth <leoroth123@yahoo.com>
2025-02-08 13:40:59 -0800

    changed forum post names to match database

--- eventaggregator/src/Pages/Forum/Forum.jsx
+++ eventaggregator/src/Pages/Forum/Forum.jsx
@@ -10,31 +10,31 @@ import thumbnail1 from '../../assets/thumbnail1.png';
 export const forumPosts = [
     {
         postId: 0,
-        event: "League LAN party at ya motha's house",
+        eventName: "League LAN party at ya motha's house",
         title: "We need to do this again guys, wasn't it fun?",
-        preview: "Man it was so fun, you guys are straight garbo ngl. But i appreciate the games and it was fun nonetheless!! much love <3",
-        author: "Pompompurin",
+        body: "Man it was so fun, you guys are straight garbo ngl. But i appreciate the games and it was fun nonetheless!! much love <3",
+        ownerName: "Pompompurin",
         timestamp: 120,
-        upvotes: 4,
-        downvotes: -31,
-        replies: 22,
-        thumbnail: thumbnail1
+        upvoteCount: 4,
+        downvoteCount: -31,
+        replyCount: 22,
+        thumbnailID: thumbnail1
     },
     {
         postId: 1,
-        event: "YFP VS Fly Quest",
+        eventName: "YFP VS Fly Quest",
         title: "Recap discussion of the League match last night. I can't believe theBausffs man! Why would he do that?",
-        preview: "What an intense match! The teamfight at Baron really turned things around. I noticed theBausffs goofing " + 
+        body: "What an intense match! The teamfight at Baron really turned things around. I noticed theBausffs goofing " + 
         "off in the top lane seemingly tricking the enemy team in his own match even though he was supposed to be casting " +
         "this game for Fly Quest, they must be very annoyed at him. Was he sponsored? If so he is most-definitely losing " +
         "the sponsorship or at least they are going to reprimand him somehow, that is just irrespopnsible to be honest. He " +
         "probably made so much money just for him to not even pay attention, like wow.",
-        author: "TheLegend27",
+        ownerName: "TheLegend27",
         timestamp: 50,
-        upvotes: 7,
-        downvotes: 1,
-        replies: 7,
-        thumbnail: thumbnail1
+        upvoteCount: 7,
+        downvoteCount: 1,
+        replyCount: 7,
+        thumbnailID: thumbnail1
     }
 ];
 
