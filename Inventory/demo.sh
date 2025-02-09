c955b4bf526c38109727ce3d47c91b38f526fad1
Drewv115 <144067980+Drewv115@users.noreply.github.com>
2025-02-09 13:43:29 -0800

    Updated Inventory
    - Created new events.csv to remove title duplicates and
    - Updated RestAPI.py with new functions to obtain events from the new dataset
    - updated demo.sh to showcase changes and request structure.

--- Inventory/demo.sh
+++ Inventory/demo.sh
@@ -1,26 +1,45 @@
-export FLASK_APP=RestAPI.py
-flask run
-
-# read -p "Press enter to continue"
-
-# # Show old items and links
-# python3 -m webbrowser http://127.0.0.1:5000/events
-
-# read -p "Press enter to continue"
-
-# # Request an update by sending the link
-curl -i http://127.0.0.1:5000/games \
+# export FLASK_APP=RestAPI.py
+# flask run
+#################################### Obtain a random event
+python3 -m webbrowser http://127.0.0.1:5000/events/random_one
+read -p "Press enter to continue"
+#################################### Obtain a random set of 3 records
+curl -i http://127.0.0.1:5000/events/random \
 -X POST \
 -H 'Content-Type: application/json' \
--d '{"Number":"8"}' 
+-d '{"NUMBER":3}' 
 
-# read -p "Press enter to continue"
+read -p "Press enter to continue"
+#################################### Dump the user metric records
+python3 -m webbrowser http://127.0.0.1:5000/dump_records
+read -p "Press enter to continue"
+####################################
+curl -i http://127.0.0.1:5000/click/record \
+-X POST \
+-H 'Content-Type: application/json' \
+-d '{"USER_ID": 456,"EVENT": "SuperHot"}' 
 
-# # Show changes
-# python3 -m webbrowser http://127.0.0.1:5000/events
+read -p "Press enter to continue"
+ECHO 'HELLO'
+#################################### Dump the user metric records
+python3 -m webbrowser http://127.0.0.1:5000/dump_records
+read -p "Press enter to continue"
+####################################
+curl -i http://127.0.0.1:5000/seen/record \
+-X POST \
+-H 'Content-Type: application/json' \
+-d '{"USER_ID":123,"EVENT": "Ratchet & Clank: Rift Apart"}' 
 
-# read -p "Press enter to continue"
+read -p "Press enter to continue"
+#################################### Dump the user metric records
+python3 -m webbrowser http://127.0.0.1:5000/dump_records
+read -p "Press enter to continue"
 
-# curl http://127.0.0.1:5000/events
 
-# read -p "Press enter to continue"
+#################################### Template
+# # Request an update by sending the link
+# curl -i http://127.0.0.1:5000/games \
+# -X POST \
+# -H 'Content-Type: application/json' \
+# -d '{"Number":"8"}' 
+####################################
