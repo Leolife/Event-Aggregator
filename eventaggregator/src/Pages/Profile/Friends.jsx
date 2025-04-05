60b9d6055f4dc964af1d6f0a79e762af109b5a65
Leo Roth <leoroth123@yahoo.com>
2025-04-05 10:29:43 -0700

    setup sidebar in friends page

--- eventaggregator/src/Pages/Profile/Friends.jsx
+++ eventaggregator/src/Pages/Profile/Friends.jsx
@@ -1,3 +1,11 @@
+import Sidebar from '../../Components/Sidebar/Sidebar';
 
+export const Friends = ({ sidebar }) => {
 
-export const Friends = ({ sidebar }) => {};
+    return (
+        <>
+            <Sidebar sidebar={sidebar} />
+            <div className={`container ${sidebar ? "" : 'large-container'}`}></div>
+        </>
+        )
+};
