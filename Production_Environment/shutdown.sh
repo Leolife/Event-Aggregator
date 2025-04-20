166e9b82acb2f4283821541b6de1bae1d2a0af07
Andrew Valdivia <Andrew.Valdivia01@student.csulb.edu>
2025-04-20 04:56:01 +0000

    Configured Dockerfile

---
+++ Production_Environment/shutdown.sh
@@ -0,0 +1,10 @@
+docker compose down
+docker images
+echo ''
+docker rm -vf $(docker ps -aq)
+docker rmi -f $(docker images -aq)
+docker images
+#echo 'Created directory'
+#sdocker compose up --build
+#  docker network prune
+# docker compose up --remove-orphans
