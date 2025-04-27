5044061563b5cc02853db5dba85163eb47074747
graminaaron <dev.aargar26@gmail.com>
2025-04-26 19:41:34 -0700

    Added text to various user subcollection
    Includes Post, Replies, Both Vote and Reply-Vote subcollections

--- eventaggregator/src/Components/Forum_post/AddForumPost.jsx
+++ eventaggregator/src/Components/Forum_post/AddForumPost.jsx
@@ -56,7 +56,7 @@ export const AddForumPost = ({ isOpen, onClose }) => {
             // Create a new document in the user's posts subcollection with the post ID and timestamp
             await setDoc(
                 doc(firestore, 'users', user.uid, 'posts', newDocRef.id),
-                { timestamp: newPostData.timestamp }
+                { timestamp: newPostData.timestamp, body: body }
             );
 
             console.log("Document written with ID:", newDocRef.id);
