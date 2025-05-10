e09ce6e52a950f9d91f087839f63eca7c107f6a6
Richie Prak <legobigchill@gmail.com>
2025-05-10 12:45:54 -0700

    Live Feed

--- eventaggregator/src/Pages/Home/Home.jsx
+++ eventaggregator/src/Pages/Home/Home.jsx
@@ -7,7 +7,7 @@ import Header from '../../Components/Header/Header'
 import { eventInfo } from './TempEvents'
 import EventTile from '../../Components/Events/CategoryCard'
 import EventCard from '../../Components/Events/EventCard';
-
+import { ReactComponent as LiveIcon } from '../../assets/live-icon.svg';
 
 
 export const Home = ({ sidebar }) => {
@@ -16,6 +16,7 @@ export const Home = ({ sidebar }) => {
   const [selectedTags, setSelectedTags] = useState([]);
   const [currentPage, setCurrentPage] = useState(1)
   const [events, setEvents] = useState([{}])
+  const [streams, setStreams] = useState([]);
   const totalPages = 5
 
   // Sort options located in the header
@@ -27,6 +28,37 @@ export const Home = ({ sidebar }) => {
   // Retrives tags from the header
   function sendData(data) {
     setSelectedTags(data)
+  }
+
+  // Convert twitch stream start time to time ago
+  function timeElapsed(startTime) {
+    const start = new Date(startTime);
+    const now = new Date();
+    const diffInSeconds = Math.floor((now - start) / 1000);
+    const diffInMinutes = Math.floor(diffInSeconds / 60);
+    const diffInHours = Math.floor(diffInMinutes / 60);
+    const diffInDays = Math.floor(diffInHours / 24);
+
+    if (diffInDays > 0) {
+      return `${diffInDays} day${diffInDays > 1 ? 's' : ''} ago`;
+    } else if (diffInHours > 0) {
+      return `${diffInHours} hour${diffInHours > 1 ? 's' : ''} ago`;
+    } else if (diffInMinutes > 0) {
+      return `${diffInMinutes} minute${diffInMinutes > 1 ? 's' : ''} ago`;
+    } else {
+      return `${diffInSeconds} second${diffInSeconds > 1 ? 's' : ''} ago`;
+    }
+  }
+
+  // Convert twitch viewer numbers to abbreviated number
+  function convertToAbbreviation(number) {
+    const formatter = new Intl.NumberFormat('en', {
+      notation: 'compact',
+      compactDisplay: 'short',
+      maximumSignificantDigits: 2
+    });
+
+    return formatter.format(number);
   }
 
   useEffect(() => {
@@ -47,6 +79,28 @@ export const Home = ({ sidebar }) => {
     const fetchedEvents = await response.json()
     setEvents(Object.values(fetchedEvents))
   }
+
+  // Fetch live events from the API
+  useEffect(() => {
+    async function fetchEvents() {
+      const response = await fetch("/streams", {
+        method: "GET",
+      });
+      const data = await response.json()
+      const width = 440;
+      const height = 248;
+      const streams = data.map((stream) => {
+        // Set the thumbnail width and height 
+        const thumbnail = `https://static-cdn.jtvnw.net/previews-ttv/live_user_${stream.user_login}-${width}x${height}.jpg`;
+        return {
+          ...stream,
+          thumbnail, // attach the new thumbnail url
+        };
+      })
+      setStreams(streams)
+    }
+    fetchEvents();
+  }, [])
 
 
 
@@ -114,10 +168,33 @@ export const Home = ({ sidebar }) => {
                 ))}
             </div>
           </div>
+          {/* Display live ongoing events*/}
           <div className='live-feed-container'>
-            <h1> 🔴 Live </h1>
+            <h1 className='live-events'> 🔴 Live Events </h1>
             <div className='live-feed'>
-
+              {streams && streams.length > 0 ? (
+                streams.map((stream, index) => (
+                  <div className='live-card'>
+                    <div className='live-card-thumbnail-container'>
+                      {/* Link to the Twitch Stream when user clicks on the thumbnail */}
+                      <a href={`https://twitch.tv/${stream.user_name}`} style={{ display: "table-cell" }} target="_blank" rel="noopener noreferrer" >
+                        <img className='live-card-thumbnail' src={stream.thumbnail} alt="" />
+                        <div className="live-box"> <LiveIcon /> LIVE </div>
+                      </a>
+                    </div>
+                    <div className='live-card-details'>
+                      <h1 className='live-card-title'> {stream.title} </h1>
+                      <label className='live-card-broadcaster'> {stream.user_name} </label>
+                      <label className='live-card-game'> {stream.game_name} </label>
+                      <label className='live-card-viewers'>  {convertToAbbreviation(stream.viewer_count)} watching • {timeElapsed(stream.started_at)}  </label>
+                    </div>
+                  </div>
+                )
+                )
+              ) : (
+                // Displays message if the streams have not loaded in
+                <label> No live events! </label>
+              )}
             </div>
           </div>
         </div>
